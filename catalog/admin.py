24e152d8e0011a9ff4a2785d6caf5a2f24db4b1b
Ksenia Panova <rybinak95@gmail.com>
2022-12-02 20:53:44 +0300

    add view for desserts in categories

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from catalog.models import Filling
 from catalog.models import Category
+from catalog.models import Dessert
 
 # Register your models here.
 admin.site.register(Filling)
 admin.site.register(Category)
+admin.site.register(Dessert)
