333727b4b9edb655e066b732ff27f71b3e2ac736
Ksenia Panova <rybinak95@gmail.com>
2022-12-07 10:56:15 +0300

    add creating review from form

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
 from django.views import generic
 from reviews.models import Review
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
+from django.utils import timezone
+from django.core.files.storage import FileSystemStorage
+import os
+from django.conf import settings
+
+upRoot = os.path.join(settings.MEDIA_ROOT, 'images/users_photos/')
+up_url = os.path.join(settings.MEDIA_URL, 'images/users_photos/')
+upRoot = upRoot.replace("\\", "/")
+
+pRoot = os.path.join(settings.MEDIA_ROOT, 'images/reviews/')
+p_url = os.path.join(settings.MEDIA_URL, 'images/reviews/')
+pRoot = pRoot.replace("\\", "/")
 
 
 class ReviewsView(generic.ListView):
@@ -9,9 +23,48 @@ class ReviewsView(generic.ListView):
 
 
 def create_new_review(request):
+    if request.method == "POST":
+        review = Review()
+        review.user_firstname = request.POST.get("user_firstname")
+        review.user_lastname = request.POST.get("user_lastname")
 
-    return render(
-        request,
-        'reviews/form_review.html',
-    )
+        files = request.FILES
+        fs1 = FileSystemStorage(location=upRoot)
+        fs2 = FileSystemStorage(location=pRoot)
+
+        filename = fs1.save(files['user_photo'].name, files['user_photo'])
+        review.user_photo = up_url.replace('/media', '')+filename
+
+        review.review_description = request.POST.get("review_description")
+        review.date = timezone.now()
+
+        if len(files) >= 2:
+            filename = fs2.save(files['photo_1'].name, files['photo_1'])
+            review.photo_1 = p_url.replace('/media', '') + filename
+
+        if len(files) >= 3:
+            filename = fs2.save(files['photo_2'].name, files['photo_2'])
+            review.photo_2 = p_url.replace('/media', '') + filename
+
+        if len(files) >= 4:
+            filename = fs2.save(files['photo_3'].name, files['photo_3'])
+            review.photo_3 = p_url.replace('/media', '') + filename
+
+        if len(files) >= 5:
+            filename = fs2.save(files['photo_4'].name, files['photo_4'])
+            review.photo_4 = p_url.replace('/media', '') + filename
+
+        if len(files) >= 6:
+            filename = fs2.save(files['photo_5'].name, files['photo_5'])
+            review.photo_5 = p_url.replace('/media', '') + filename
+
+        review.save()
+        return HttpResponseRedirect(reverse('reviews'))
+    else:
+        return render(
+            request,
+            'reviews/form_review.html',
+        )
+
+
 
